29e436b473561963402bb4ed0eb8711daa419658
deiiivy <deivypr28@gmail.com>
2025-06-19 14:46:26 -0500

    Se crea configuracion de contenedores docker

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,15 +1,16 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) => res.send('API Usocial funcionando'));
+app.get('/', (_req, res) => {
+  res.send('API Usocial funcionando correctamente');
+});
 
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
