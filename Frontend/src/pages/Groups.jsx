4ed2c1fa61a972813908e2e760805e87a96ba71e
Deiiivy <deivypr28@gmail.com>
2025-09-02 11:29:27 -0500

    Initializing page Groups

--- Frontend/src/pages/Groups.jsx
+++ Frontend/src/pages/Groups.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Aside from '../components/Aside.jsx'
+import Header from '../components/Header.jsx'
 
 const Grupos = () => {
   return (
-    <div>Grupos</div>
+    <div className='flex flex-col items-center mt-28 max-h-[500px] w-screen overflow-y-hidden'>
+      <Header />
+      <Aside />
+      <div className='flex flex-col w-full max-w-4xl mx-auto pl-12 overflow-y-hidden'>
+        <h1>Grupos</h1>
+      </div>
+    </div>
   )
 }
 
